64a59875f92a91ab5520ed1a39b828459ed27af3
igors <spitkovskyigor@gmail.com>
2022-02-19 21:25:14 +0200

    renamed services

--- src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -1,17 +1,18 @@
 package guru.springframework.sfgdi.services;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by igors on 2/19/22
  */
-@Profile({"ES", "default"})
+@Profile("EN")
 @Service("i18nService")
-public class I18nSpanishService implements GreetingService{
+public class I18nEnglishGreetingService implements GreetingService{
 
     @Override
     public String sayGreeting() {
-        return "Hola Mundo - ES";
+        return "Hello World - EN";
     }
 }
--- src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
@@ -1,18 +1,17 @@
 package guru.springframework.sfgdi.services;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by igors on 2/19/22
  */
-@Profile("EN")
+@Profile({"ES", "default"})
 @Service("i18nService")
-public class I18nEnglishService implements GreetingService{
+public class I18nSpanishGreetingService implements GreetingService{
 
     @Override
     public String sayGreeting() {
-        return "Hello World - EN";
+        return "Hola Mundo - ES";
     }
 }
