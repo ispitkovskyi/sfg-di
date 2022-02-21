6b5a630be7c4747e971bd9f6e4da28c2e2e8c16a
igors <spitkovskyigor@gmail.com>
2022-02-21 17:51:39 +0200

    life-cycle-demo

--- src/main/java/guru/springframework/sfgdi/services/I18nSpanishService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nSpanishService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
  */
 @Profile({"ES", "default"})
 @Service("i18nService")
-public class I18nSpanishGreetingService implements GreetingService{
+public class I18nSpanishService implements GreetingService{
 
     @Override
     public String sayGreeting() {
