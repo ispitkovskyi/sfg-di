89a9a1b888916cd813b359f560ceab2f1abef57d
ispitkovskyi <spitkovskyigor@gmail.com>
2022-07-08 21:53:42 +0300

    comment added

--- src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
@@ -15,6 +15,7 @@ public class SetterInjectedController {
 
     private GreetingService greetingService;
 
+    //@Qualifier("setterInjectedGreetingService") //Can also be here instead of being in-line with method declaration
     @Autowired
     public void setGreetingService(@Qualifier("setterInjectedGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
