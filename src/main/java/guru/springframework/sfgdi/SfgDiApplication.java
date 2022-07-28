2342275172bca4ab0db5ce47e3f5c3bdd27a2157
ispitkovskyi <spitkovskyigor@gmail.com>
2022-07-28 11:28:54 +0300

    custom packages to be scanned by Spring

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -4,7 +4,10 @@ import guru.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+//Configure which root-packages Spring scan should look into (by default it looks only into current package and packages underneath)
+@ComponentScan(basePackages = {"guru.springframework.sfgdi", "com.springframework.pets"})
 @SpringBootApplication
 public class SfgDiApplication {
 
@@ -13,6 +16,9 @@ public class SfgDiApplication {
 		//Spring scans project for classes, annotated as Spring components and creates instances of bin-classes,
 		//and puts created instances into a Spring context
 		// NO NEED to explicitly create instance of MyController class
+		// NOTE: Spring will scan classes, located on the same level with this class AND in al packages sarting from this level down
+		// So, Spring will be looking into this "guru.springframework.sfgdi" pakcage and all the packages underneath
+		// NOTE: Any classes, located outside the package, which this class belongs to, will NOT be scanned
 		//By default when Spring creates a bin, the bin name will be same as the class name, but in camel-case style:
 		PetController petController = ctx.getBean("petController", PetController.class);
 		System.out.println("--- The Best Pet is ---");
